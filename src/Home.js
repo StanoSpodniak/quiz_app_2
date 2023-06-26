23c1e4b1af2f542d8adbd57b6675a0015ad2b034
StanoSpodniak <stano28@gmail.com>
2023-06-26 20:33:12 +0200

    Getting random questions from external API

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,42 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+//https://the-trivia-api.com/docs/v2/#tag/Questions/operation/getRandomQuestionshttps://the-trivia-api.com/docs/v2/#tag/Questions/operation/getRandomQuestions
 
 const Home = () => {
-    const [quizzes, setData] = useState([
-        { title: 'Capital Cities', id: 1 },
-        { title: 'History', id: 2 }
-    ]);
+    const [data, setData] = useState(null);
+    const url = "https://the-trivia-api.com/v2/questions/?difficulties=easy&limit=1&categories=geography";
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setData(data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    };
 
     return ( 
         <div className="home">
-            <h2>All quizzes</h2>
+            <h2>Question</h2>
+            <div>
+            </div>
             <div className="quiz">
-                {quizzes.map((quiz) => (
-                    <Link><h2>{quiz.title}</h2></Link>
-                ))}
+                {/* This will be moved to another file*/}
+                {data ? (
+                    <p>{data[0].question.text}</p>
+                ) : (
+                    <p>Loading...</p>
+                )}
+                {data ? (
+                    <p>{data[0].correctAnswer}</p>
+                ) : (
+                    <p>Loading...</p>
+                )}
             </div>
         </div>
      );
