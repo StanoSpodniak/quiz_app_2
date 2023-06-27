522138c3def53c384ceef8a86f32938226ca41eb
StanoSpodniak <stano28@gmail.com>
2023-06-27 13:55:32 +0200

    Populated options

--- src/Home.js
+++ src/Home.js
@@ -2,41 +2,60 @@ import { useState, useEffect } from 'react';
 //https://the-trivia-api.com/docs/v2/#tag/Questions/operation/getRandomQuestionshttps://the-trivia-api.com/docs/v2/#tag/Questions/operation/getRandomQuestions
 
 const Home = () => {
-    const [data, setData] = useState(null);
     const url = "https://the-trivia-api.com/v2/questions/?difficulties=easy&limit=1&categories=geography";
+    const [data, setData] = useState(null);
 
-    useEffect(() => {
+    useEffect(() => {   
+        const fetchData = () => {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    setData(data)
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        };
+        
         fetchData();
     }, []);
 
-    const fetchData = () => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                setData(data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    };
-
     return ( 
         <div className="home">
-            <h2>Question</h2>
+            <h2>Question:</h2>
             <div>
             </div>
             <div className="quiz">
-                {/* This will be moved to another file*/}
                 {data ? (
-                    <p>{data[0].question.text}</p>
+                    <p id="question-text">{data[0].question.text}</p>
                 ) : (
                     <p>Loading...</p>
                 )}
-                {data ? (
-                    <p>{data[0].correctAnswer}</p>
-                ) : (
-                    <p>Loading...</p>
-                )}
+                <div className="options">
+                    {data ? (
+                        <button>{data[0].correctAnswer}</button>
+                    ) : (
+                        <button>Loading...</button>
+                    )}
+
+                    {data ? (
+                        <button>{data[0].incorrectAnswers[0]}</button>
+                    ) : (
+                        <button>Loading...</button>
+                    )}
+
+                    {data ? (
+                        <button>{data[0].incorrectAnswers[1]}</button>
+                    ) : (
+                        <button>Loading...</button>
+                    )}
+                    
+                    {data ? (
+                        <button>{data[0].incorrectAnswers[2]}</button>
+                    ) : (
+                        <button>Loading...</button>
+                    )}
+                </div>
             </div>
         </div>
      );
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return ( 
     <nav className="navbar">
-        <Link to="/"><h1>Free Quizzes</h1></Link>
+        <Link to="/"><h1>Free Quiz</h1></Link>
         <div className="links">
             <Link to="/" className="link">Home</Link>
             <Link to="/create" className="link">Create Quiz</Link>
